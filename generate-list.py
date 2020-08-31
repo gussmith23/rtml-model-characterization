0d4faa2794b79993a34a6ac69c459474e43a9899
Gus Smith <guscomps@gmail.com>
2020-08-31 16:21:16 -0700

    Add more networks

--- generate-list.py
+++ generate-list.py
@@ -1,8 +1,17 @@
 import tvm
 from tvm import relay
 from tvm.relay import analysis_tools
-from tvm.relay.testing import resnet
 import pandas as pd
+from tvm.relay.testing import mlp
+from tvm.relay.testing import resnet
+from tvm.relay.testing import dqn
+from tvm.relay.testing import dcgan
+from tvm.relay.testing import mobilenet
+from tvm.relay.testing import lstm
+from tvm.relay.testing import inception_v3
+from tvm.relay.testing import squeezenet
+from tvm.relay.testing import vgg
+from tvm.relay.testing import densenet
 
 
 class GetReadableName(analysis_tools.AnalysisPass):
@@ -38,6 +47,16 @@ summary_columns = set()
 for (module, _), name in [
     (resnet.get_workload(num_layers=18), 'resnet18'),
     (resnet.get_workload(num_layers=50), 'resnet50'),
+    (mobilenet.get_workload(), 'mobilenet'),
+    (mlp.get_workload(batch_size=1), 'mlp'),
+    (dqn.get_workload(batch_size=1), 'dqn'),
+    (dcgan.get_workload(batch_size=1), 'dcgan'),
+        # LSTM throws an error w/ analysis framework
+        #    (lstm.get_workload(iterations=32, num_hidden=32), 'lstm'),
+    (inception_v3.get_workload(), 'inception_v3'),
+    (squeezenet.get_workload(), 'squeezenet'),
+    (vgg.get_workload(batch_size=1), 'vgg'),
+    (densenet.get_workload(), 'densenet'),
 ]:
     program = module['main']
     analyses = [GetReadableName(), GetIndex(), SummarizeOpTypes()]
